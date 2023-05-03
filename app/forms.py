c35e80aec31baa654c81e521563f2f2673179835
Nikita Samsonenkov <mrkimatsuki@gmail.com>
2023-05-03 18:03:40 +0500

    Commit #3

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, TextAreaField, FileField, SubmitField
+from wtforms.validators import Optional, DataRequired
 
+
+class MovieForm(FlaskForm):
+    title = StringField("Название",
+                        validators=[DataRequired(message='Поле "Название" не может быть пустым')])
+    description = TextAreaField("Описание",
+                                validators=[DataRequired(message='Поле "Описание" не может быть пустым')])
+    img = FileField("Изображение", validators=[Optional()])
+    submit = SubmitField("Добавить")
+
+
+class ReviewForm(FlaskForm):
+    name = StringField("Имя",
+                        validators=[DataRequired(message='Поле "Имя" не может быть пустым')])
+    text = TextAreaField("Отзыв",
+                         validators=[DataRequired(message='Поле "Отзыв" не может быть пустым')])
+    rating = SelectField("Оценка", choices=list(range(10, -1, -1)), default=10)
+    submit = SubmitField("Добавить")
--- app/views.py
+++ app/views.py
@@ -1,8 +1,15 @@
-from flask import render_template, redirect
+from flask import render_template, redirect, url_for
 
-# from .forms import MovieForm, ReviewForm
+from .forms import MovieForm, ReviewForm
 from .models import Movie, Review
 from . import app, db
+
+from pathlib import Path
+from werkzeug.utils import secure_filename
+
+
+BASEP = Path(__file__).parent
+UPLOAD_FOLDER = BASEP / 'static' / 'images'
 
 
 @app.route("/")
@@ -14,19 +21,59 @@ def index():
 
 @app.route("/movie/<int:id>", methods=["GET", "POST"])
 def movie(id):
-    return "Страница с фильмом"
+    movie_inf = Movie.query.get(id)
+    form = ReviewForm()
+    reviews_list = movie_inf.review
+    avg = round(sum([i.rating for i in reviews_list]) / len(reviews_list), 1)
+
+    if form.validate_on_submit():
+        new_review = Review()
+        new_review.name = form.name.data
+        new_review.text = form.text.data
+        new_review.rating = form.rating.data
+        new_review.movie_id = id
+
+        db.session.add(new_review)
+        db.session.commit()
+
+    return render_template("movie.html", movie_inf=movie_inf, avg=avg, form=form)
 
 
 @app.route("/add_movie", methods=["GET", "POST"])
 def add_movie():
-    return "Добавление фильма"
+    form = MovieForm()
+
+    if form.validate_on_submit():
+        new_movie = Movie()
+        new_movie.title = form.title.data
+        new_movie.description = form.description.data
+
+        img = form.img.data
+        img_name = secure_filename(img.filename)
+        UPLOAD_FOLDER.mkdir(exist_ok=True)
+        img.save(UPLOAD_FOLDER / img_name)
+
+        new_movie.img = img_name
+
+        db.session.add(new_movie)
+        db.session.commit()
+
+        return redirect(url_for("index"))
+
+    return render_template("add_movie.html", form=form)
 
 
 @app.route("/reviews")
 def reviews():
-    return "Страница с отзывами"
+    reviews_list = Review.query.all()
+
+    return render_template("reviews.html", reviews_list=reviews_list)
 
 
 @app.route("/delete_review/<int:id>")
 def delete_review(id):
-    return "Удаление новости"
+    review_d = Review.query.get(id)
+    db.session.delete(review_d)
+    db.session.commit()
+
+    return redirect(url_for("reviews"))
